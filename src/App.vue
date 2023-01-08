e1d1a8884a964d94677927acb1874ff685418177
panduola666 <panduola666@gmail.com>
2023-01-08 23:59:57 +0800

    修改成功

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>vite 測試成功</h1>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
